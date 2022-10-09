d7fb98450cec02233122f02ddb10e4e7519dce7e
DorukEmre <doruk.emre@outlook.com>
2022-10-09 17:32:58 +0100

    Add task button only appears on root page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
   // const onClick = (event) => {
   //   console.log(event.type, 'at', event.pageX, event.pageY)
   // }
+  const location = useLocation()
+
+  // Show button only if we are in route '/'
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? 'red' : 'green'}
-        text={showAdd ? 'Close' : 'Add'}
-        onClick={onAdd}
-      />
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
     </header>
   )
 }
